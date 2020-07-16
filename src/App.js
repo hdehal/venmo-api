14585fc35a348e727a1c2d90a6124c1068d65c8a
hdehal <harry@hdehal.com>
2020-07-16 18:15:25 +0000

    Fetch JSON from Venmo API

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import './App.css';
 
 class App extends React.Component {
 
-  state = {
-    receipts: []
+  // Initial state
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      receipts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://venmo.com/api/v5/public?limit=1000')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ receipts: data })
+      })
+      .catch(console.log)
   }
 
   render() {

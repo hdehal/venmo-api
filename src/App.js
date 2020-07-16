cb674fc7612b5ec180edc8eb6bac526d7f40f7f8
hdehal <harry@hdehal.com>
2020-07-16 20:38:09 +0000

    CORS Anywhere testing

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch('https://venmo.com/api/v5/public?limit=1000')
+    // Using the public CORS-Anywhere vs local deployment for testing
+    fetch('https://cors-anywhere.herokuapp.com/https://venmo.com/api/v5/public')
       .then(res => res.json())
       .then((data) => {
-        this.setState({ receipts: data })
+        this.setState({ contacts: data })
       })
-      .catch(console.log)
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err);
+      })
   }
 
   render() {

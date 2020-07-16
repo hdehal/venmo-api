a5c5ea649d6de5c084d96974182a7ba315ce0e26
hdehal <harry@hdehal.com>
2020-07-16 23:53:57 +0000

    Add CSS animation to cards

--- src/components/receipts.js
+++ src/components/receipts.js
@@ -10,7 +10,7 @@ class Receipts extends Component {
 
         {this.props.receipts.map((receipt, i) => (
           <div id="receiptsContainer" className="center">
-            <Card>
+            <Card style={{ "--animation-order": i }}>
               <Card.Subtitle><strong>{receipt.actor.name}</strong> paid <strong>{receipt.transactions[0].target.name}</strong></Card.Subtitle>
               <Card.Body>{receipt.message}</Card.Body>
             </Card>

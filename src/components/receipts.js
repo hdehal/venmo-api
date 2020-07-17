e58ed29ef282dbbb1f75639fa4bc8def556b527c
hdehal <harry@hdehal.com>
2020-07-17 07:20:33 +0000

    Randomly generated dollar values for demo

--- src/components/receipts.js
+++ src/components/receipts.js
@@ -10,9 +10,19 @@ class Receipts extends Component {
 
         {this.props.receipts.map((receipt, i) => (
           <div id="receiptsContainer" className="center">
-            <Card style={{ "--animation-order": i }}>
-              <Card.Subtitle><strong>{receipt.actor.name}</strong> paid <strong>{receipt.transactions[0].target.name}</strong></Card.Subtitle>
-              <Card.Body>{receipt.message}</Card.Body>
+            <Card style={{ "--animation-order": i }} className="toast">
+              <Card.Subtitle>
+                {/* Real fake names to protect the innocent via Venmo public API */}
+                <strong>{receipt.actor.firstname}</strong> paid
+                <strong> {receipt.transactions[0].target.firstname} </strong>
+                {/* Fake cash monies for demo purposes */}
+                <span className="alert-success">
+                  {"$" + (Math.floor(Math.random() * (1000 - 100) + 100) / 10).toFixed(2)}
+                </span>
+              </Card.Subtitle>
+              <Card.Body>
+                {receipt.message}
+              </Card.Body>
             </Card>
           </div>
         ))}

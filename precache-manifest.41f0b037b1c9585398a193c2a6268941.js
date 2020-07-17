42655f563235826ffc8192c14e5d0ee86ed30f98
hdehal <harry@hdehal.com>
2020-07-17 00:14:20 +0000

    Updates

---
+++ precache-manifest.41f0b037b1c9585398a193c2a6268941.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc29e32a4d651b6f4f3d45d3fd3bf8b7",
+    "url": "/venmo-api/index.html"
+  },
+  {
+    "revision": "74e2082e71f1b8b47539",
+    "url": "/venmo-api/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "710525c7e4f29b670d3f",
+    "url": "/venmo-api/static/css/main.d71750ae.chunk.css"
+  },
+  {
+    "revision": "74e2082e71f1b8b47539",
+    "url": "/venmo-api/static/js/2.a98cbbd8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/venmo-api/static/js/2.a98cbbd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "710525c7e4f29b670d3f",
+    "url": "/venmo-api/static/js/main.b607c5fc.chunk.js"
+  },
+  {
+    "revision": "1e9a6e3bf394965accea",
+    "url": "/venmo-api/static/js/runtime-main.7c90445d.js"
+  }
+]);
